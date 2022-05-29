4878b9477b641c4854d6d76102fd935fa5422a71
Alexis <flexibeast@gmail.com>
2022-05-29 22:59:28 +1000

    Update copyright year.

--- ebuku.el
+++ ebuku.el
@@ -1,6 +1,6 @@
 ;;; ebuku.el --- Interface to the buku Web bookmark manager -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019-2021  Alexis <flexibeast@gmail.com>, Erik Sjöstrand <sjostrand.erik@gmail.com>, Junji Zhi [https://github.com/junjizhi]
+;; Copyright (C) 2019-2022  Alexis <flexibeast@gmail.com>, Erik Sjöstrand <sjostrand.erik@gmail.com>, Junji Zhi [https://github.com/junjizhi]
 
 ;; Author: Alexis <flexibeast@gmail.com>, Erik Sjöstrand <sjostrand.erik@gmail.com>, Junji Zhi [https://github.com/junjizhi]
 ;; Maintainer: Alexis <flexibeast@gmail.com>
